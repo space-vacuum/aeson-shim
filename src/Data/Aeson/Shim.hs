cd4441016d1946f689ddee6969b9877515610afa
Phil de Joux <philderbeast@gmail.com>
2022-10-12 21:01:21 -0400

    Switch aeson on ghc versions.

--- src/Data/Aeson/Shim.hs
+++ src/Data/Aeson/Shim.hs
@@ -10,8 +10,8 @@ module Data.Aeson.Shim
 import qualified Data.Aeson.Key as Key
 import qualified Data.Aeson.KeyMap as KM
 import Data.HashMap.Strict (HashMap)
-#endif
 import Data.Text (Text)
+#endif
 
 -- SEE: https://github.com/phadej/aeson-optics/commit/ff31ca0578482df11002f0cde974ff51b8559e1c
 #if MIN_VERSION_aeson(2,0,0)
